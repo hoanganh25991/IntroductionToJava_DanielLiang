f1eb33a24c132d454b4721a26c9152c27fd0f26e
Le Hoang Anh <hoanganh_25991@yahoo.com.vn>
2015-07-02 15:31:53 +0700

    change name

---
+++ src/JavaTestOnline/TreeNode02.java
@@ -0,0 +1,46 @@
+package JavaTestOnline;
+
+import java.util.List;
+
+public class TreeNode02 {
+
+	// Please do not change this interface
+	public static interface Node {
+		int getValue();
+
+		List<Node> getChildren();
+	}
+
+	public static double getAverage(Node root) {
+		/*
+		 * Please implement this method to return the average of all node values (Node.getValue()) in the tree.
+		 */
+		double average = 0.0;
+		return average;
+	}
+	static class MyNode implements Node{
+		int size;
+		Node root;
+		public MyNode() {
+			// TODO Auto-generated constructor stub
+		}
+		public MyNode(int value){
+			
+		}
+		
+		@Override
+		public int getValue() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public List<Node> getChildren() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+}
+
+
